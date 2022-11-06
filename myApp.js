1d2853acb3d37882364eccf278724465add9657f
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-11-06 13:06:23 +0000

    2022年11月06日 - 01

--- myApp.js
+++ myApp.js
@@ -1,19 +1,33 @@
 let express = require('express');
 let app = express();
 
+/* Lesson01
+app.get('/', (req, res) => {
+  console.log('Hello World');
+})
+*/
 
+/* Lesson02
+app.get('/', (req, res) => {
+  res.send('Hello Express');
+})
+*/
 
+/* Lesson03
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+})
+*/
 
+/* Lesson04
+app.use(express.static(__dirname + '/'));
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+})
+*/
 
-
-
-
-
-
-
-
-
+/* Lesson05 */
 
 
 
